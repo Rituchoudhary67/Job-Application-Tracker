389ef0c6ae1851a86bf964c88a9769e025be5b00
Ritu Choudhary <114747083+Rituchoudhary67@users.noreply.github.com>
2025-12-28 15:16:34 +0530

    database added

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express();
+const pool = require("./config/db");
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res) => {
-    res.send("Job Application Tracker API is running");
+app.get("/", (req, res) => {
+  res.send("Job Application Tracker API is running");
+});
+
+app.get("/db-test", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json({ dbTime: result.rows[0].now });
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
